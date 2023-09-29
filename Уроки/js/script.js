61897b79678f940cf38b93520e37f340f13687d1
Evgenij <evgenijvolobuev5@gmail.com>
2023-09-29 19:32:16 +0300

    cliner

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -1,17 +1,2 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-console.log(number);
-let arr = ['plum', 'orange', 'apple'];
-console.log(arr[1]);
-
-let a = 0;
-
-console.log(a);
-a++;
-console.log(a);
-let e;
-e = 10;
