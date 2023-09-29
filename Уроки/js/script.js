f65587316a5cb3a3f971d43fb4d78f2f07781857
Evgenij <evgenijvolobuev5@gmail.com>
2023-09-30 00:23:46 +0300

    exercise 2, study 15

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num = 0;
+/*let num = 0;
 
 if (num > 0){
     console.log('norm');
@@ -9,7 +9,7 @@ if (num > 0){
 }else if(num === 0){
     console.log('bad');
 }
-
+*/
 // 1 //12 ==========================================================================
 /*let numberOfFilms = +prompt( 'Сколько фильмов вы посмотрели', '');
 
@@ -29,3 +29,39 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 */
+// 2/15======================================================
+
+let numberOfFilms = +prompt( 'Сколько фильмов вы посмотрели', '');
+if(numberOfFilms != '' && numberOfFilms != null){
+     
+    if(numberOfFilms < 10){
+        console.log(' Просмоьренно мало фильмов');
+    }else if(numberOfFilms >= 10 && numberOfFilms <= 30){
+        console.log('Вы классный зритель');
+    }else if(numberOfFilms > 30){
+        console.log('Задрот');
+    }
+}else{ 
+    console.log( 'Error');
+}     
+    
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i ++ ){
+    const a = prompt('Один из последних фильмов?' , ''),
+          b = prompt('На солько оцениваете его?', '');
+          if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+            personalMovieDB.movies[a] = b;
+          }else{
+            console.log('error');
+            i--;
+          }    
+}
+
+console.log(personalMovieDB);
