c349970c92cd1d630a7e3e54624e9cb70fe3b466
Evgenij <evgenijvolobuev5@gmail.com>
2023-09-29 22:41:58 +0300

    practic цикл и условие

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -1,2 +1,36 @@
 "use strict";
+/*let a = 11;
 
+if (a > 0){
+    console.log('Nice');
+}else if(a < 0){
+    console.log('Fack')
+}else{
+    console.log('Bad');
+}
+
+switch(a){
+    case 0:
+        console.log('ziro')
+        break;
+    case 1:
+        console.log('Adyn');
+        break;
+    default:
+        console.log('blet!');
+        break;        
+}*/
+
+for(let i = 2; i < 8; i= i + 2){
+    console.log(i);
+}
+let a = 1;
+
+while (a < 5){
+    if( a == 3){
+        continue;
+    }
+    console.log(a);
+    a ++;
+}
+//=============================================================
