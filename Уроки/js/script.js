555edbde05ded394bfbcb425ad5def32d93603cd
Evgenij <evgenijvolobuev5@gmail.com>
2023-09-29 22:02:43 +0300

    exercise 1 , study 12

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -9,3 +9,23 @@ if (num > 0){
 }else if(num === 0){
     console.log('bad');
 }
+
+// 1 //12 ==========================================================================
+/*let numberOfFilms = +prompt( 'Сколько фильмов вы посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних фильмов?' , ''),
+      b = prompt('На солько оцениваете его?', ''),
+      c = prompt('Один из последних фильмов?' , ''),
+      d = prompt('Один из последних фильмов?' , '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+*/
