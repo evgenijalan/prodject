fb1c12316117f232cdf01e76d2ccf7b07fe187bd
Evgenij <evgenijvolobuev5@gmail.com>
2023-09-29 18:36:31 +0300

    first exercise

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -7,3 +7,21 @@ number = 10;
 console.log(number);
 let arr = ['plum', 'orange', 'apple'];
 console.log(arr[1]);
+
+let numberOfFilms = +prompt( 'Сколько фильмов вы посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних фильмов?' , ''),
+      b = prompt('На солько оцениваете его?', ''),
+      c = prompt('Один из последних фильмов?' , ''),
+      d = prompt('Один из последних фильмов?' , '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
