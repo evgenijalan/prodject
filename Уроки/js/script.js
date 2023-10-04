8de502882d81d7d7dae8c1352aa8977bfad57a3a
Evgenij <evgenijvolobuev5@gmail.com>
2023-10-04 23:17:30 +0300

    additional: function деструктуризация (дополнение)

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -1,2 +1,33 @@
 "use strict";
 
+//function деструктуризация ========================
+/*
+
+
+function calcValue(a, b){
+    return [
+        a + b,
+        a - b,
+        a * b,
+        a / b,
+    ]
+}
+
+const [sum,, mult] = calcValue( 2, 5);
+console.log(sum, mult);
+
+const personal = {
+    name: 'Max',
+    age: 20,
+    adress: {
+        country: 'Russian',
+        city: 'Moscow',
+    },
+};
+
+//const {name} = personal;
+//console.log(name);
+
+const {name: firstName, age, car = 'Машины нет', adress: {city}} = personal;
+console.log(firstName,age, car, city);
+*/
