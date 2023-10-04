5c6266a2801d94e2242b929b2f404c3bdd0cc34b
Evgenij <evgenijvolobuev5@gmail.com>
2023-10-05 00:58:14 +0300

    additional: function деструктуризация (дополнение2)

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //function деструктуризация ========================
+//--------------------------------------------------
 /*
 
 
@@ -15,6 +16,7 @@ function calcValue(a, b){
 
 const [sum,, mult] = calcValue( 2, 5);
 console.log(sum, mult);
+//----------------------------------------------
 
 const personal = {
     name: 'Max',
@@ -30,4 +32,41 @@ const personal = {
 
 const {name: firstName, age, car = 'Машины нет', adress: {city}} = personal;
 console.log(firstName,age, car, city);
+
+//-------------------------------------------------
+
+const auto = ['tesla', 2006, 'green', 20];
+const [name, year, ...sw] = auto;
+console.log(sw)
+console.log( name, year, sw);
+
+const [color, age, count = 10] = sw;
+console.log(color, age, count);
+
+
+//-------------------------------------------------
+
+function conveerRubles(rubles){
+    const complete = {
+        eur: rubles * 80,
+        usd: rubles * 70,
+        gbr: rubles * 100,
+    };
+
+    return complete;
+}
+const {eur, usd} = conveerRubles(5000);
+console.log(eur, usd);
+
+//const rrr = conveerRubles(1000);
+//console.log(rrr);
 */
+//-------------------------------------------------
+
+function getTotal({ total = 100, shipment = 0.1} = {}){
+    return total + total * shipment;
+}
+const total = getTotal({total: 1000, shipment : 0.2});
+console.log(total);
+const total1 = getTotal();
+console.log(total1);
