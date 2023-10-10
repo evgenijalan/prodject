6a968450e95c76db0b4eb34df6aa1ba9eca102e8
Evgenij <evgenijvolobuev5@gmail.com>
2023-10-10 20:32:03 +0300

    study: действие с элементами на странице

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -424,3 +424,51 @@ console.log(oneHeart);
 //действие с элементами на странице=============================================
 //------------------------------------------------------------------------------
 
+/*
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circle = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
+
+box.style.background = 'blue'; 
+box.style.width = '500px'; 
+
+box.style.cssText = 'background-color: red';
+
+btns[1].style.borderRadius = '100%';
+circle[0].style.backgroundColor = 'red';
+
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = 'blue';
+}
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'pink';
+});
+
+const div = document.createElement('div');
+//const text = documemt.createTextNode('this i am');
+
+div.classList.add('black');
+document.body.append(div);
+//document.querySelector('.wrapper').append(div);
+//wrapper.append(div);
+//wrapper.prepend(div);
+//hearts[0].before(div);
+//hearts[0].after(div);
+//circle[0].remove();
+//hearts[0].replaceWith(circle[0]);
+
+// устаревшее 
+//wrapper.appendChild(div);
+//wrapper.insertBefore(div, hearts[0]);
+//wrapper.removeChild(hearts[1]);
+//wrapper.replaceChild(circle[0], hearts[0]);
+
+// работа с текстом
+div.innerHTML = '<h1>Hello world</h1>';
+//div.textContent = 'hello world';
+div.insertAdjacentHTML('beforeBegin', '<h2>hello</h2>');
+*/
