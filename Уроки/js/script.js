9178f3bbbd8be9fed1eaa5fea05534c52f6e05d5
Evgenij <evgenijvolobuev5@gmail.com>
2023-10-10 14:39:49 +0300

    study: Получение элементов со страници dom

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -390,3 +390,33 @@ if (switcher) {
 //2)
 console.log(typeof(Boolean('4')));
 */
+
+//Получение элементов со страници=================================================
+//--------------------------------------------------------------------------------
+
+/*
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+
+console.log(btns);
+console.log(btns[2]);
+
+const btn = document.getElementsByTagName('button')[1];
+console.log(btn);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+*/
