6edbfc75fecd77787d7702461b89774dfa0fc2fa
Evgenij <evgenijvolobuev5@gmail.com>
2023-10-17 23:23:31 +0300

    study: 032 Навигация по DOM - элементам, data-атрибуты, преимущество for_of

--- Уроки/js/script.js
+++ Уроки/js/script.js
@@ -472,3 +472,84 @@ div.innerHTML = '<h1>Hello world</h1>';
 //div.textContent = 'hello world';
 div.insertAdjacentHTML('beforeBegin', '<h2>hello</h2>');
 */
+//события и их обработчики===========================================================================================
+//--------------------------------------------------------------------------------------------------------------------
+/*
+const btn = document.querySelector('button'),
+      btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//     alert('click');
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+
+// btn.addEventListener('mouseenter', (e) => {
+//     e.target.remove();
+//     console.log('Hover');
+// });
+
+// btn.addEventListener('click', (e) => {
+//     e.target.remove();
+//     console.log('Hover');
+// });
+
+let i = 0;
+const deleteElement = (e) => {
+    //console.log(e.target);
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if(i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+btn.addEventListener('click', deleteElement);
+//btn.removeEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement);
+
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    console.log(e.target);
+});
+
+btns.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        console.log(e.target);
+    });
+});
+*/
+
+//032 Навигация по DOM - элементам, data-атрибуты, преимущество for_of=========================
+//------------------------------------------------------------------------------------------
+//ПОЛУЧЕНИЕ УЗЛОВ
+//console.log(document.body);
+// console.log(document.documentElement);
+//console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('[data-current]').nextSibling);
+// console.log(document.querySelector('[data-current]').previousSibling);
+
+//ПОЛУЧЕНИЕ ЭЛЕМЕНТОВ
+// console.log(document.querySelector('[data-current]').nextElementSibling);
+// console.log(document.querySelector('#current').parentElement);
+// console.log(document.body.firstElementChild);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
+
